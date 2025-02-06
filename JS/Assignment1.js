09370dceff87b5fbc3df9b94f3d1b19ae79abb9d
Krati900 <krati7898@gmail.com>
2025-02-06 09:06:29 +0530

    Js Assigmnent- 01

---
+++ JS/Assignment1.js
@@ -0,0 +1,73 @@
+// Create a number variable with some value, now print the good number if no is divisible by 10 and print bad number if not. 
+
+// let num = 20;
+// if(num % 10 == 0) {
+//   console.log("Good Number");
+// }
+// else {
+//   console.log("Bad Number");
+// }
+
+// Take user name and age as input using prompt and display in alert.
+
+// let Name = prompt("Enter your Name: ");
+// let Age = prompt("Enter you Age: ");
+// let display = console.log(`User Name is ${Name} and Age is ${Age}`);
+// alert(`User Name is ${Name} and Age is ${Age}`);
+
+// Write a switch statement to print the months in a quarter. 
+
+// let Quarter = prompt("Enter the Quarter Number Like any of them 1,2,3,4");
+
+// switch (Quarter) {
+//   case '1': alert("Month will be: \nJanuary\nFebruary\nMarch");
+//             break;
+//   case '2': alert("Month will be: \nApril\nMay\nJune");
+//             break;
+//   case '3': alert("Month will be: \nJuly\nAugust\nSeptember");
+//             break;
+//   case '4': alert("Month will be: \nOctober\nNovember\nDecember");
+//             break;
+//   default: alert("OOPS.....! No any month exist in this Quarter");
+// }
+
+// A string is a golden string if it is start with a 'A and 'a' and has a total length greater then 5 if it has then this golden or not. 
+
+// let str = "Tanisha";
+
+// if((str.length > 5) && (str[0] == 'a' || str[0] == 'A')) {
+//   alert("This is Golden String");
+// }
+// else {
+//   alert("Not a Golden string");
+// }
+
+// To write a program to find out the largest among three numbers 
+
+// let a = -90;
+// let b = 56787;
+// let c = 999;
+
+// if(a>b) {
+//   if(a>c) {
+//     console.log("a is greater");
+//   }
+//   else {
+//     console.log("c is greater");
+//   }
+// }
+// else {
+//   console.log("b is greater");
+// }
+
+// Write a program to check if 2 numbers have the same last digit. Eg. 32 and 47852 have the same last digit 2. 
+
+// let num1 = 234467;
+// let num2 = 688544;
+
+// if(num1 % 10 === num2 % 10) {
+//   console.log("Both numbers have same digit, " + num1%10);
+// }
+// else {
+//   console.log("Both numbers have not the same digit, " + num1%10 +  " " + num2%10);
+// }
